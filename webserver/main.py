fffdb5ee591237686b12b238f8f4385691c7b6dd
jonharod <jonharod@DESKTOP-4AHVQ92>
2023-04-04 10:41:23 -0500

    Instalacion de fastapi en webserver

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Este es el titulo</h1>
+        <p>Esta es una prueba de FastApi</p>
+    """
 
 def run():
     store.get_categories()
